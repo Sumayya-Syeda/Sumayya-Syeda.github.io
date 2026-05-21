4885a7e727b5db134f70834600a8df9596dd531b
Sumayya Syeda <sumayya.syeda@gmail.com>
2026-05-21 00:12:24 -0700

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://sumayya-syeda.github.io',
   base: '/website',
-  integrations: [tailwind()],
 });
